ec8d62f9e569ffcc3159e665a8da1782e5607c7f
jvirissimo <jvirissimo@gmail.com>
2020-05-05 17:08:41 -0500

    8 files modified, 3 added, 3 deleted

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss'],
+  host:{
+    role: 'banner'
+  }
 })
 export class HeaderComponent implements OnInit {
 
