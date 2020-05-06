ff7c47713a5c5e2c4ae128688edc819cb261aff9
jvirissimo <jvirissimo@gmail.com>
2020-05-06 10:57:30 -0500

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { LayoutModule } from './layout/layout.module';
 import { LoginModule } from './login/login.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { AppComponent } from './app.component';
 
-
-// const routes: Routes = [
-//   {
-//     path: '',
-//     redirectTo: '/dashboard',
-//     pathMatch: 'full'
-//   }
-// ];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, LoginModule, DashboardModule ],
+  imports:      [ BrowserModule, FormsModule, LayoutModule, LoginModule, DashboardModule, RouterModule.forRoot(routes) ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
